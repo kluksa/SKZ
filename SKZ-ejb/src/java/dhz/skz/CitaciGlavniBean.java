aa00a42156029b31e173a9ee67223a0ad8745f82
Luksa Kraljevic <kraljevic@cirus.dhz.hr>
2016-04-27 16:23:39 +0200

    Razvoj iox - zero span

--- SKZ-ejb/src/java/dhz/skz/CitaciGlavniBean.java
+++ SKZ-ejb/src/java/dhz/skz/CitaciGlavniBean.java
@@ -88,8 +88,7 @@ public class CitaciGlavniBean extends Scheduler implements CitaciGlavniBeanRemot
 
                 InitialContext ctx = new InitialContext();
                 String str = "java:module/";
-//                for (IzvorPodataka ip : izvorPodatakaFacade.getAktivniIzvori()) {
-                IzvorPodataka ip = izvorPodatakaFacade.find(10);
+                for (IzvorPodataka ip : izvorPodatakaFacade.getAktivniIzvori()) {
                     String naziv = str + ip.getBean().trim();
                     log.log(Level.INFO, "JNDI: {0}", naziv);
                     try {
@@ -99,7 +98,7 @@ public class CitaciGlavniBean extends Scheduler implements CitaciGlavniBeanRemot
                         log.log(Level.SEVERE, "POGRESKA KOD CITANJA IZVORA {0}:{1}", new Object[]{ip.getId(), ip.getNaziv()});
                         log.log(Level.SEVERE, null, ex);
                     }
-//                }
+                }
                 minutniUSatne.napraviSatne(0);
             } catch (NamingException ex) {
                 log.log(Level.SEVERE, null, ex);
